598a88837cd49504f06903a5e9172ae09b513494
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-03-26 14:00:36 -0600

    landingpage

--- pages/landingpage.vue
+++ pages/landingpage.vue
@@ -1,14 +1,26 @@
+<script>
+import blogsVue from './blogs.vue';
+  export default {
+    methods: {
+      blogPage() {
+        this.$router.push(blogsVue);
+      }
+    }
+  }
+</script>
+
 <template>
   <div id="l-page">
     <div id="info">
       <img src="../public//Logo.png" alt="logo-travel-blog">
     </div>
-    <button type="button">BLOGS!</button>
+    <button @click="blogPage">BLOGS!</button>
   </div>
 </template>
 
 <style scoped>
   #l-page {
+    margin: 22vh 22vh 22vh 22vh;
     flex-direction: column;
   }
 
@@ -17,6 +29,20 @@
   }
 
   button {
+    background: linear-gradient(#5472cc, #538eae);
+    font-weight: 500;
+    color: #d4e3f0;
+    border-radius: 2px;
+    box-shadow: 2px 2px 0 #8e9dcc;
+    border: none;
     margin-top: 22px;
+    height: 35px;
+    width: 15vh;
+    cursor: pointer;
+  }
+
+  button:hover{
+    color: #294598;
+    border-radius: 5px;
   }
 </style>

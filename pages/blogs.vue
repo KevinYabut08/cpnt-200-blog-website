8d00ea64d2c7b7634f251648200ed38f8f6ff671
KevinYabut08 <Kevin.Yabut@edu.sait.ca>
2024-03-27 03:01:46 +0800

    added pages

---
+++ pages/blogs.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Blogs Page</h1>
+    <p>This is the blogs page content.</p>
+  </div>
+</template>
---
+++ pages/sign-up.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Sign-up Page</h1>
+    <p>This is the sign-up page content.</p>
+  </div>
+</template>

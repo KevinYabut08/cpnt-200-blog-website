716d0ea9e024b56cd62b472eb207befa56e43053
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-04-03 19:56:30 -0600

    fixing landing page and dynamic pages

--- components/Hero.vue
+++ components/Hero.vue
@@ -11,16 +11,6 @@
   </div>
   <div>
   </div>
-
-  <div>
-    <p>
-      Welcome to Hidden Gems! This is your go-to travel blog website full of joy and adventures. Whether you are seeking inspiration <br>
-      for your next getaway or practical tips for navigating new destinations, we've got you covered. Hidden Gems is a virtual treasure <br>
-      trove of firsthand travel experiences, expert recommendations, and captivating stories from globetrotters around the world.
-    </p>
-  </div>
-
-  <button type="button"><NuxtLink to="blog">Blogs</NuxtLink></button>
 </NuxtLayout>
 </template>
 <style scoped>
--- pages/landing-page.vue
+++ pages/landing-page.vue
@@ -7,5 +7,22 @@
 <template>
   <main>
     <Hero /> 
+
+    <div>
+    <p>
+      Welcome to Hidden Gems! This is your go-to travel blog website full of joy and adventures. Whether you are seeking inspiration
+      for your next getaway or practical tips for navigating new destinations, we've got you covered. Hidden Gems is a virtual treasure
+      trove of firsthand travel experiences, expert recommendations, and captivating stories from globetrotters around the world.
+    </p>
+  </div>
+
+  <button type="button"><NuxtLink to="blog">Blogs</NuxtLink></button>
+
   </main>
 </template>
+<style scoped>
+  p {
+    justify-content: center;
+    text-align: center;
+  }
+</style>

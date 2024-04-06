59abc3d26c07f9a1b97e1cec2e0c350f6be3cf7d
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-04-05 21:48:28 -0600

    question mark

--- pages/articles/post/[post].vue
+++ pages/articles/post/[post].vue
@@ -1,22 +1,27 @@
-<script>
+<script setup>
 const route = useRoute();
-const supabase = useSupabaseClient();
-console.log(route.params.post);
-const { data: post } = await useAsyncData("post", async () => {
-  const { data } = await supabase
+ const supabase = useSupabaseClient();
+  console.log(route.params.blog);
+ const { data: blog } = await useAsyncData("blog", async () => {
+   const { data } = await supabase
   .from("blogs")
   .select("*")
-  .eq("slug", route.params.post)
+   .eq("slug", route.params.blog)
   .single();
   return data;
-})
+ })
 </script>
 <template>
-  <main>
-    <h1>{{ post.title }}</h1>
-    <p>{{ post.content }}</p>
-    <p>Blog Last Update: {{ post.date_updated }}</p>
-  </main>
+  <div v-if="blog.value">
+    <h2> {{ blog.value.title }}</h2>
+    <p> {{ blog.value.description }}</p>
+    <p>Blog Last Update: {{ blog.date_updated }}</p>
+  </div>
+  <!-- <main>
+    <h1>{{ blog.title }}</h1>
+    <p>{{ blog.content }}</p>
+    <p>Blog Last Update: {{ blog.date_updated }}</p>
+  </main> -->
 </template>
 <style>
 main {

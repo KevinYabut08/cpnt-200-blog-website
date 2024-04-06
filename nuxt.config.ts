fccb6231101609a1b10455aeeedc1d2a53e30829
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-04-05 21:39:58 -0600

    nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/supabase"],
+  modules: ["@nuxtjs/supabase", "@nuxtjs/eslint-module"],
   supabase: { redirect: false },
 });
